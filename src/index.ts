7b861cd615e320f913af8d8e8cdc44f222e10133
Georges Gomes <georges.gomes@gmail.com>
2019-09-12 15:21:25 +0200

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
 import parse from "./parser";
 import { injectToJavaScript, injectStylesToJavaScript } from "./injector";
 
-function sfc2esm(sfc: string) : string {
-
+function sfc2esm(sfc: string): string {
   const parts = parse(sfc);
 
-  if (parts.script.lang!=='javascript')
-  {
+  if (parts.script.lang !== "javascript") {
     throw new Error("Only Javascript is currently supported");
   }
 
-  let esm = parts.script.source || 'export default {}';
-  esm = injectToJavaScript(parts.template || '', esm);
+  let esm = parts.script.source || "export default {}";
+  esm = injectToJavaScript(parts.template || "", esm);
 
-  esm = injectStylesToJavaScript("__sfc2esm__styles", parts.styles, esm);
-  esm = injectStylesToJavaScript("__sfc2esm__styles_scoped", parts.styles_scoped, esm);
+  if (parts.styles.length > 0) {
+    esm = injectStylesToJavaScript("__sfc2esm__styles", parts.styles, esm);
+  }
+
+  if (parts.styles_scoped.length > 0) {
+    esm = injectStylesToJavaScript(
+      "__sfc2esm__styles_scoped",
+      parts.styles_scoped,
+      esm
+    );
+  }
 
   return esm;
 }
